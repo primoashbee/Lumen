a44df01a28fffdd2a4214b5e0a9d0a2048b530ec
nelz07 <nelsontan1128@gmail.com>
2021-08-21 09:07:11 +0800

    Revert "Merge pull request #36 from nelz07/create-deposit-accounts"
    This reverts commit 915a3cc9012aae3b6ec26f6028ec2fead7cb3844, reversing
    changes made to 78eb969e105d3fde2b3ec88f00510d91aac7a248.

--- resources/js/components/ClientCreateLoanAccountComponent.vue
+++ resources/js/components/ClientCreateLoanAccountComponent.vue
@@ -226,6 +226,12 @@ export default {
 			})
 			.catch(err =>{
 				this.errors = error.response.data.errors || {}
+				Swal.fire({
+					icon: 'warning',
+					title: '<p style="color:green;font-size:1em;font-weight:bold">OOPPPSSSSS</p>',
+					text: this.errors,
+				})
+				
 			})
 		},
 		getDate(value,field){
@@ -243,16 +249,12 @@ export default {
 			})
 			.catch(err=>{
 				this.errors = err.response.data.errors || {}
-				 var html="";
 				$.each(this.errors, function(k, v){ 
 					html += '<p class="text-left">'+ v +'</p>'
 				})
 				Swal.fire({
 						icon: 'error',
-						title: '<span style="font-family:\'Open Sans\', sans-serif!important;color:red;font-size:1em;font-weight:600">Something went wrong!</span>',
-						html: html +'</ul>'
-					})
-				
+
 				this.calculated = false;
 			});
 			
