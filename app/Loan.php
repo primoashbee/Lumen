8746f1433fc2a559560b3bd49a73516072de4cc3
nelz07 <nelsontan1128@gmail.com>
2021-09-06 13:00:18 +0800

    Update Loan

--- app/Loan.php
+++ app/Loan.php
@@ -498,7 +498,7 @@ class Loan extends Model
                                 (object) [
                                     'code'=>'GML',
                                     'installments'=>12,
-                                    'rate'=>   3.59520,
+                                    'rate'=>  3.57620,
                                     'number_of_months'=> 6
                                 ],
                                 // (object) [
@@ -615,148 +615,148 @@ class Loan extends Model
                         'code'=>'RF-GML',
                         'rates'=>
                             collect([
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>2,
-                                    'rate'=> 2.85086,
-                                    'number_of_months'=>1
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>4,
-                                    'rate'=>  3.40176 ,
-                                    'number_of_months'=>2
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>6,
-                                    'rate'=> 3.42289,
-                                    'number_of_months'=>3
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>8,
-                                    'rate'=> 3.48432 ,
-                                    'number_of_months'=>4
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>10,
-                                    'rate'=>  3.59616,
-                                    'number_of_months'=> 5
-                                ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>2,
+                                //     'rate'=> 2.85086,
+                                //     'number_of_months'=>1
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>4,
+                                //     'rate'=>  3.40176 ,
+                                //     'number_of_months'=>2
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>6,
+                                //     'rate'=> 3.42289,
+                                //     'number_of_months'=>3
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>8,
+                                //     'rate'=> 3.48432 ,
+                                //     'number_of_months'=>4
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>10,
+                                //     'rate'=>  3.59616,
+                                //     'number_of_months'=> 5
+                                // ],
                                 (object) [
                                     'code'=>'GML',
                                     'installments'=>12,
-                                    'rate'=>   3.58166,
+                                    'rate'=>   3.57620,
                                     'number_of_months'=> 6
                                 ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>14,
-                                    'rate'=>  3.59151 ,
-                                    'number_of_months'=> 7
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>16,
-                                    'rate'=>  3.59849,
-                                    'number_of_months'=> 8
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>18,
-                                    'rate'=> 3.61478,
-                                    'number_of_months'=> 9
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>20,
-                                    'rate'=>   3.60954,
-                                    'number_of_months'=> 10
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>22,
-                                    'rate'=>  3.59896,
-                                    'number_of_months'=> 11
-                                ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>14,
+                                //     'rate'=>  3.59151 ,
+                                //     'number_of_months'=> 7
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>16,
+                                //     'rate'=>  3.59849,
+                                //     'number_of_months'=> 8
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>18,
+                                //     'rate'=> 3.61478,
+                                //     'number_of_months'=> 9
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>20,
+                                //     'rate'=>   3.60954,
+                                //     'number_of_months'=> 10
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>22,
+                                //     'rate'=>  3.59896,
+                                //     'number_of_months'=> 11
+                                // ],
                                 (object) [
                                     'code'=>'GML',
                                     'installments'=>24,
-                                    'rate'=>   3.59461,
+                                    'rate'=>   3.59520,
                                     'number_of_months'=> 12
                                 ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>26,
-                                    'rate'=> 3.58336,
-                                    'number_of_months'=> 13
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>28,
-                                    'rate'=> 3.58098,
-                                    'number_of_months'=> 14
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>30,
-                                    'rate'=> 3.56868,
-                                    'number_of_months'=> 15
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>32,
-                                    'rate'=> 3.55538,
-                                    'number_of_months'=> 16
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>34,
-                                    'rate'=> 3.54498,
-                                    'number_of_months'=> 17
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>36,
-                                    'rate'=> 3.53338,
-                                    'number_of_months'=> 18
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>38,
-                                    'rate'=> 3.52138,
-                                    'number_of_months'=> 19
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>40,
-                                    'rate'=> 3.50866,
-                                    'number_of_months'=> 20
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>42,
-                                    'rate'=> 3.49638,
-                                    'number_of_months'=> 21
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>44,
-                                    'rate'=> 3.48338,
-                                    'number_of_months'=> 22
-                                ],
-                                (object) [
-                                    'code'=>'GML',
-                                    'installments'=>46,
-                                    'rate'=> 3.47098,
-                                    'number_of_months'=> 23
-                                ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>26,
+                                //     'rate'=> 3.58336,
+                                //     'number_of_months'=> 13
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>28,
+                                //     'rate'=> 3.58098,
+                                //     'number_of_months'=> 14
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>30,
+                                //     'rate'=> 3.56868,
+                                //     'number_of_months'=> 15
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>32,
+                                //     'rate'=> 3.55538,
+                                //     'number_of_months'=> 16
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>34,
+                                //     'rate'=> 3.54498,
+                                //     'number_of_months'=> 17
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>36,
+                                //     'rate'=> 3.53338,
+                                //     'number_of_months'=> 18
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>38,
+                                //     'rate'=> 3.52138,
+                                //     'number_of_months'=> 19
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>40,
+                                //     'rate'=> 3.50866,
+                                //     'number_of_months'=> 20
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>42,
+                                //     'rate'=> 3.49638,
+                                //     'number_of_months'=> 21
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>44,
+                                //     'rate'=> 3.48338,
+                                //     'number_of_months'=> 22
+                                // ],
+                                // (object) [
+                                //     'code'=>'GML',
+                                //     'installments'=>46,
+                                //     'rate'=> 3.47098,
+                                //     'number_of_months'=> 23
+                                // ],
                                 (object) [
                                     'code'=>'GML',
                                     'installments'=>48,
-                                    'rate'=> 3.45908,
+                                    'rate'=> 3.45894,
                                     'number_of_months'=> 24     
                                 ],
                             ]) 
