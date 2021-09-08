e96b882cf1fe2629d44ae32de70f439fba0dcd57
nelz07 <nelsontan1128@gmail.com>
2021-09-08 09:31:57 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')->hourly();
-        $schedule->command('loan:update')->cron('* * * * *');
+        $schedule->command('loan:update')->everyMinute();
         $schedule->command('par:calculate')->cron('00 1 * * *');
         $schedule->command('word:day')->cron('* * * * *')->appendOutputTo(public_path('/output.txt'));
         // $schedule->command('command:test')->everyMinute();
