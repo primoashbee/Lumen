0627635b13916923b2df1f3fce82c1ecd4b2818c
nelz07 <nelsontan1128@gmail.com>
2021-10-02 15:43:28 +0800

    Merge

--- app/LoanAccount.php
+++ app/LoanAccount.php
@@ -1158,6 +1158,7 @@ class LoanAccount extends Model
 
     public function updateStatus()
     {
+        
         $x = 0;
         if (is_null($this->disbursed_by)){
             return $this->update([
@@ -1600,36 +1601,37 @@ class LoanAccount extends Model
     {
         $amount = $this->preTermAmount();
         // $transaction_id = $this->generateRepaymentTransactionNumber('pretermination');
-        
+
         $paid_by = $data['paid_by'];
         $payment_method_id = $data['payment_method_id'];
         $repayment_date = $data['repayment_date'];
         $notes = $data['notes'];
         $office_id = $data['office_id'];
         $jv_number = $data['jv_number'];
-        
+
         $method = PaymentMethod::find($data['payment_method_id']);
-        
         if ($method->isCTLP()) {
                 $type = 'CTLP';
                 $ctlp_account = $this->client->ctlpAccount();
-                
-                $transaction_number = $this->generateRepaymentTransactionNumber('pretermination');
+                $transaction_number = 'X'.str_replace('.','',microtime(true));
+            
+                //withdraw from ctlp account
                 
                 $data['transaction_number'] = $transaction_number;
                 $withdrawal = $ctlp_account->payCTLP($data);
                 //get withdrawal transcation from deposit account
-                // dd($withdrawal);
-                $transaction = Transaction::create([
-                    'transaction_number'=>$transaction_number,
-                    'type'=>'Withdawal - CTLP',
-                    'office_id'=>$office_id,
-                    'transaction_date'=>$data['repayment_date'],
-                    'posted_by'=>$paid_by
-                ]);
-                
+                // $transaction = $withdrawal->transaction()->create([
+                //     'transaction_number'=>$transaction_number,
+                //     'type'=>'Withdawal - CTLP',
+                //     'office_id'=>$office_id,
+                //     'transaction_date'=>$data['repayment_date'],
+                //     'posted_by'=>$paid_by
+                // ]);
+
                 //create a repayment
                 $deposit_to_loan_repayment = $this->fromDepositPayments()->create([
+                    'transaction_number' => $transaction_number,
+                    'office_id' => $office_id,
                     'interest_paid'=>0,
                     'principal_paid'=>0,
                     'total_paid'=>0,
@@ -1648,14 +1650,13 @@ class LoanAccount extends Model
                     'total_paid' => $repayments->total_paid
                     ]);
                 $deposit_to_loan_repayment = $deposit_to_loan_repayment->fresh();
-                
-                $deposit_to_loan_repayment->transaction()->create([
-                    'transaction_number'=>$transaction_number,
-                    'type'=>$type,
-                    'transaction_date'=>$repayment_date,
-                    'posted_by'=>$paid_by,
-                    'office_id'=>$office_id
-                ]);
+                // $deposit_to_loan_repayment->transaction()->create([
+                //     'transaction_number'=>$transaction_number,
+                //     'type'=>$type,
+                //     'transaction_date'=>$repayment_date,
+                //     'posted_by'=>$paid_by,
+                //     'office_id'=>$office_id
+                // ]);
 
                 $deposit_to_loan_repayment->jv()->create([
                     'journal_voucher_number'=>$jv_number,
