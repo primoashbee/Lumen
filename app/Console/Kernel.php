d0571b95de2dde3ff17cd1add21c44ea384d6ccb
nelz07 <nelsontan1128@gmail.com>
2022-03-01 06:54:36 +0800

    Change Recal Dues Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,12 +29,12 @@ class Kernel extends ConsoleKernel
     {
 
         // $schedule->command('inspire')->hourly();
-        $schedule->command('loan:update')->cron('01 0 * * *')->appendOutputTo(public_path('/output.txt'));
+        $schedule->command('loan:update')->cron('0 7 * * *')->appendOutputTo(public_path('/output.txt'));
         $schedule->command('deposit:accrue')->cron('30 2 * * *')->appendOutputTo(public_path('/output.txt'));
         // $schedule->command('par:calculate')->cron('01 3 * * *');
         $schedule->command('holiday:implement')->cron('0 16 * * *')->appendOutputTo(public_path('/output.txt'));
         // $schedule->command('word:day')->cron('* * * * *')->appendOutputTo(public_path('/output.txt'));
-        // $schedule->command('command:test')->everyMinute();
+        // $schedule->command('command:test')->everyMin ute();
     }
 
     /**
