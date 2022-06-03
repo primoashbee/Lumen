07e2738c41cbf59866bd4ecab23bf75a74dcedbf
nelz07 <nelsontan1128@gmail.com>
2022-06-03 11:21:59 +0800

    Update RecalculateLoanDues.php

--- app/Console/Commands/RecalculateLoanDues.php
+++ app/Console/Commands/RecalculateLoanDues.php
@@ -59,49 +59,68 @@ class RecalculateLoanDues extends Command
             $this->info('Starting....');
             $this->info('Date is ' . now()->toDateString());
             $lai = \DB::table('loan_account_installments');
+    
             $loan_accounts_installments_repayments = DB::table('loan_account_installment_repayments')
-            ->select('principal_paid','interest_paid','total_paid','loan_account_installment_id')
+            ->select('principal_paid','interest_paid','total_paid','penalty_paid','loan_account_installment_id')
             ->leftJoinSub($lai,'loan_account_installment', function($join){
                 $join->on('loan_account_installment.id','loan_account_installment_repayments.loan_account_installment_id');
             });
             
 
             $deposit_accounts_installments_repayments = \DB::table('deposit_to_loan_installment_repayments')
-            ->select('principal_paid','interest_paid','total_paid','loan_account_installment_id')
+            ->select('principal_paid','interest_paid','total_paid','penalty_paid','loan_account_installment_id')
             ->leftJoinSub($lai,'loan_account_installment', function($join){
                 $join->on('loan_account_installment.id','deposit_to_loan_installment_repayments.loan_account_installment_id');
             });
 
-            $payments =  $loan_accounts_installments_repayments->unionAll($deposit_accounts_installments_repayments);
+            $payments =  $loan_accounts_installments_repayments->union($deposit_accounts_installments_repayments)
+                ->select(
+                DB::raw('SUM(principal_paid) as principal_paid'),
+                DB::raw('SUM(interest_paid) as interest_paid'),
+                DB::raw('SUM(penalty_paid) as penalty_paid'),
+                DB::raw('SUM(total_paid) as total_paid'),
+                'loan_account_installment_id')
+                ->groupBy('loan_account_installment_id');
+            
+                
 
-            $lai = \DB::table('loan_account_installments')
-            ->select(
-                'installment',
-                'amount_due',
-                'date','amortization',
-                'principal','interest',
-                'principal_due',
-                'interest_due',
-                'original_interest',
-                'original_principal',
-                DB::raw('loan_account_installments.id AS installment_id'),
-                DB::raw('SUM(payments.principal_paid) AS total_principal_paid'),
-                DB::raw('SUM(payments.interest_paid) AS total_interest_paid'),
-            )
-            ->leftJoinSub($payments, 'payments', function($join){
-                $join->on('loan_account_installments.id','payments.loan_account_installment_id');
-            })
-            ->groupBy('installment_id')
-            ->orderBy('installment','asc')
-            ->whereDate('date','<=', now())
-            ->where('paid',false)
-            ->update(
-                [
-                    'interest_due' => DB::raw('round(original_interest - IFNULL(payments.interest_paid,0),2)'),
-                    'principal_due' => DB::raw('round(original_principal - IFNULL(payments.principal_paid,0),2)'),
-                    'amount_due' => DB::raw('round(interest_due+principal_due,2)')
-                ]
-            );
+                $lai = \DB::table('loan_account_installments')
+                ->select(
+                    'installment',
+                    'amount_due',
+                    'date','amortization',
+                    'principal','interest',
+                    'principal_due',
+                    'interest_due',
+                    'original_interest',
+                    'original_principal',
+                    DB::raw('loan_account_installments.id AS installment_id')
+                )
+                ->leftJoinSub($payments, 'payments', function($join){
+                    $join->on('loan_account_installments.id','payments.loan_account_installment_id');
+                })
+                ->orderBy('installment','asc')
+                ->whereDate('date','<=', now())
+                ->where('paid',false)
+                ->groupBy('loan_account_installments.id')
+                ->update(
+                        [
+                            'interest_due' => DB::raw('round(original_interest - IFNULL(payments.interest_paid,0),2)'),
+                            'principal_due' => DB::raw('round(original_principal - IFNULL(payments.principal_paid,0),2)'),
+                            'amount_due' => DB::raw('round(round(original_principal - IFNULL(payments.principal_paid,0),2) + 
+                            round(original_interest - IFNULL(payments.interest_paid,0),2),2)')
+                        ]
+                    );
+                // ->update(
+                //     [
+                //         'interest_due' => DB::raw('round(original_interest - IFNULL(payments.interest_paid,0),2)'),
+                //         'principal_due' => DB::raw('round(original_principal - IFNULL(payments.principal_paid,0),2)'),
+                //         'amount_due' => DB::raw('round(round(original_principal - IFNULL(payments.principal_paid,0),2) + 
+                //         round(original_interest - IFNULL(payments.interest_paid,0),2) +
+                //         round(penalty - IFNULL(payments.penalty_paid,0),2)
+                //         ,2)')
+                //     ]
+                // );
 
             $accounts = LoanAccount::active()->chunk(2000, function($loans){
                 foreach($loans as $loan){
